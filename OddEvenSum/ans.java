96b6e11358179286ce5ed191a9b98b597e918c8f
K-Sandeep07 <kunigirisandeep99@gmail.com>
2026-06-13 18:56:09 +0530

    Add Java program to sum odd and even numbers
    Implement a program to calculate the sum of odd and even numbers from user input.

---
+++ OddEvenSum/ans.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Main. */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        int odd = 0;
+        int even = 0;
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+            if(arr[i]%2==0)
+                even += arr[i];
+            else
+                odd += arr[i];
+        }
+        System.out.println(odd+" "+even);
+    }
+}
